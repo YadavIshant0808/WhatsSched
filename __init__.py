7246053018ac8e02f5f51dea920b19ef19365170
Ishant Yadav <ishantyadav08082007@gmail.com>
2024-11-24 13:03:37 +0530

    intial commit

---
+++ __init__.py
@@ -0,0 +1,12 @@
+"""
+WhatsApp Scheduler: Schedule and send WhatsApp messages with retry and error handling.
+
+Author: Ishant Yadav
+License: MIT
+"""
+
+__version__ = "1.0.0"
+__author__ = "Ishant Yadav"
+
+# Import the main function
+from .WhatsSched import send_whatsapp_message
